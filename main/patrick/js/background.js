df84b158511c56160bcfd146ffeeb419509d259d
Patrick Robbins <m167141162@gmail.com>
2018-04-03 12:46:51 -0400

    improved background mechanics & expanded content

--- main/patrick/js/background.js
+++ main/patrick/js/background.js
@@ -1,6 +1,27 @@
+bg_dir = .1;
+bg_dir_c = 0;
 function render_background(){//render anything meant to be displayed on the background.
 	ctx.drawImage(door,2080-current_offset_x,440);
 	ctx.drawImage(door,2240-current_offset_x,440);
 	ctx.drawImage(door,2920-current_offset_x,440);
 	ctx.drawImage(door,3160-current_offset_x,440);
+	ctx.globalAlpha=0.5;//faded out for background images (non-foreground),above is anything thats not on the same level as the player,
+	ctx.drawImage(tree1,660-current_offset_x,445);//but still apart of the main area, (like doors)
+	ctx.drawImage(tree2,190-current_offset_x,445);
+	ctx.drawImage(tree1,120-current_offset_x,445);
+	ctx.drawImage(tree2,1060-current_offset_x,445);
+	ctx.drawImage(tree1,2760-current_offset_x,445);
+	ctx.drawImage(tree2,2425-current_offset_x,445);
+	ctx.drawImage(tree1,1160-current_offset_x,445);
+	ctx.drawImage(tree2,1860-current_offset_x,445);
+	if (bg_dir_c > 250){bg_dir = -.1;};
+	if (bg_dir_c < 0){bg_dir = .1;};
+	bg_dir_c += bg_dir;
+	ctx.drawImage(cloud1,(190-current_offset_x)+bg_dir_c,140,220,110);
+	ctx.drawImage(cloud2,(790-current_offset_x)+bg_dir_c,185,200,100);
+	ctx.drawImage(cloud3,(1090-current_offset_x)+bg_dir_c,175,280,95);
+	ctx.drawImage(cloud2,(2190-current_offset_x)+bg_dir_c,200,200,100);
+	ctx.drawImage(cloud1,(1790-current_offset_x)+bg_dir_c,135,220,140);
+	ctx.drawImage(cloud3,(2590-current_offset_x)+bg_dir_c,90,200,100);
+	ctx.globalAlpha=1;
 }
--- main/patrick/js/functions.js
+++ main/patrick/js/functions.js
@@ -37,75 +37,10 @@ function render_world(){
 		}
 	}
 }
-function update_player(){//update the players position.
-player.current_action="standing";
-player.i_y -=.8;
-player.i_x = 0;
-if(keysDown[39]){player.facing="right";player.i_x = 5;};
-if(keysDown[37]){player.facing="left";player.i_x = -5;};
-if(keysDown[38]){if(player.jumpable==1){player.i_y +=19;player.jumpable=0;}};
-if(player.x < 200){player.x += 5;current_offset_x -=5;}
-if(player.x > 600){player.x -= 5;current_offset_x +=5;}
-if (player.i_x >0){//trying to go right
-	for(i=0;i<(player.i_x);i++){
-	if((game_world[Math.floor((player.y+5)/40)][Math.floor((player.x + 31 + current_offset_x)/40)].type ==0) && (game_world[Math.floor((player.y+35)/40)][Math.floor((player.x + 31 + current_offset_x)/40)].type ==0)){
-		player.x +=1;
-		player.current_action="walking";
-	}
-	}
-}else if(player.i_x <0){//going left.
-	for(i=0;i< -player.i_x;i++){
-		if((game_world[Math.floor((player.y+5)/40)][Math.floor(((player.x - 1) + current_offset_x)/40)].type ==0) && (game_world[Math.floor((player.y+35)/40)][Math.floor(((player.x - 1) + current_offset_x)/40)].type ==0)){
-		player.x -=1;
-		player.current_action="walking";
-	}
-	}
-}
-if (player.i_y >0){//going up
-	for(i=0;i<(player.i_y);i++){
-		if (player.y>3){
-	if((game_world[Math.floor((player.y-1)/40)][Math.floor((player.x + 5 + current_offset_x)/40)].type ==0) && (game_world[Math.floor((player.y-1)/40)][Math.floor((player.x + 25 + current_offset_x)/40)].type ==0)){
-		player.y -=1;
-		player.current_action="jumping";
-		}else{
-			player.i_y = 0;
-			block_hit();//test and run any actions associated with hitting the blocks.
-		}
-		}else{player.i_y=0;}
-	}
-}else if(player.i_y <0){//going down
-	for(i=0;i< -player.i_y;i++){
-		if((game_world[Math.floor((player.y+41)/40)][Math.floor(((player.x + 5) + current_offset_x)/40)].type ==0) && (game_world[Math.floor((player.y+41)/40)][Math.floor(((player.x + 25) + current_offset_x)/40)].type ==0)){
-		player.y +=1;
-		player.current_action="jumping";
-		player.jumpable = 0;
-	}else{
-		player.jumpable = 1;
-		player.i_y = -1;
-	}
-}
-}
-}
 function block_hit(){//called when the player has jumped up into a block.
 	block_hit_sound.play();//eventually we can change sound per block type.
 	game_world[Math.floor((player.y-2)/40)][Math.floor((player.x + 3 + current_offset_x)/40)].affect_hit();
 	game_world[Math.floor((player.y-2)/40)][Math.floor((player.x + 27 + current_offset_x)/40)].affect_hit();
-}
-function render_player(){//render the player, using the state data in the player object. handles animations & stuff.
-	current+=.3;
-	if (current>7){current=0;};
-	if (player.facing == "left"){
-	ctx.scale(-1,1);
-	if (player.current_action=="walking"){
-	ctx.drawImage(playerc,25*Math.floor(current),1,25,43,-player.x-30,player.y,30,40);}else{
-		ctx.drawImage(playerc,25*7,1,25,43,-player.x-30,player.y,30,40);
-	}
-	ctx.scale(-1,1);//change the scale back
-	}else{//facing right
-	if (player.current_action=="walking"){
-	ctx.drawImage(playerc,25*Math.floor(current),1,25,43,player.x,player.y,30,40);
-	}else{ctx.drawImage(playerc,25*7,1,25,43,player.x,player.y,30,40);}
-	}
 }
 function render_score(){//display the current score.
 ctx.font = "30px Arial";
