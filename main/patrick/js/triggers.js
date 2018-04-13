81bc1954fd0f166ea9c7e27ae478f9f5db011419
JadenKorr42 <36278915+JadenKorr42@users.noreply.github.com>
2018-04-13 12:58:17 -0400

    Experimental Level transition
    *Added EXIT block to tileset
    *Changed World array to be dependant on current_game_world variable
    *Added function to change current_game_world to the level 1 map
    
    *Currently non-functional

--- main/patrick/js/triggers.js
+++ main/patrick/js/triggers.js
@@ -6,6 +6,7 @@ game_world[12][56].affect_hit=function(){player.x-=160;door_open_sound.play();};
 game_world[12][73].affect_hit=function(){player.x+=240;door_open_sound.play();};//triggers
 game_world[12][79].affect_hit=function(){player.x-=240;door_open_sound.play();};
 game_world[11][78].affect_hit=function(){this.affect_hit=function(){};this.type=4;game_world[9][72].type=1;game_world[9][72].affect_hit=function(){score_block();this.type=4;this.affect_hit =function(){}};game_world[9][71].type=1;game_world[9][71].affect_hit=function(){score_block();this.type=4;this.affect_hit =function(){}};game_world[9][70].type=1;game_world[9][70].affect_hit=function(){score_block();this.type=4;this.affect_hit =function(){}};};
+game_world[11][87].affect_hit=function(){go_level1()};
 function switch_bg(){
 	if(last_call <-2){last_call=1;
 	if(bg_col==1){document.getElementById("gameCanvas").style.backgroundColor = "lightblue";bg_col=2;
